c0d5789effdff64f78b7591235c1019c5d953dc1
VanThen60hz <nguyenvthang2409@gmail.com>
2023-11-27 21:25:33 +0700

    add code

--- ss3/ss3-practice/src/App.js
+++ ss3/ss3-practice/src/App.js
@@ -1,13 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
-import AddComponent from "./components/AddComponent";
+import { Component } from "react";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <AddComponent firstNumber={1} secondNumber={2} />
-    </div>
-  );
+  increase = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  decrease = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", padding: 30 }}>
+        <button onClick={this.increase}>Increase</button>
+        <span style={{ padding: 10 }}> {this.state.number} </span>
+        <button onClick={this.decrease}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
