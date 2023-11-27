9391b23fd1a5e3fc312c7238fd7f5873e95360f8
VanThen60hz <nguyenvthang2409@gmail.com>
2023-11-27 10:45:06 +0700

    add code

---
+++ ss1/practice/p1_promise/main.ts
@@ -0,0 +1,23 @@
+let money = 10000;
+
+const buyCar = (car: any) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (money >= 1000) {
+        resolve("can buy " + car);
+      } else {
+        reject("Do not enough money");
+      }
+    }, 2000);
+  });
+};
+
+money = 1000001;
+
+const promise = buyCar("VinFast")
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
