7e389356c47907d77cc172c288fb1647f3e561ec
VanThen60hz <nguyenvthang2409@gmail.com>
2023-11-28 11:52:17 +0700

    prac-change-background

--- ss3/ss3-practice/src/App.js
+++ ss3/ss3-practice/src/App.js
@@ -3,24 +3,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 0,
+      color: "black",
     };
   }
 
-  increase = () => {
-    this.setState({ number: this.state.number + 1 });
-  };
-
-  decrease = () => {
-    this.setState({ number: this.state.number - 1 });
-  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ color: "pink" });
+    }, 3000);
+  }
 
   render() {
     return (
-      <div style={{ textAlign: "center", padding: 30 }}>
-        <button onClick={this.increase}>Increase</button>
-        <span style={{ padding: 10 }}> {this.state.number} </span>
-        <button onClick={this.decrease}>Decrease</button>
+      <div>
+        <div
+          style={{
+            backgroundColor: this.state.color,
+            paddingTop: 20,
+            width: 400,
+            height: 80,
+            margin: "auto",
+          }}
+        />
       </div>
     );
   }
