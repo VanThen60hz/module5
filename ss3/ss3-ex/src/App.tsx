ef7f55509c5d567c0543be7efb6ea06b4ec2de39
VanThen60hz <nguyenvthang2409@gmail.com>
2023-12-01 20:28:51 +0700

    add code ss3-ex-loginform

--- ss3/ss3-ex/src/App.tsx
+++ ss3/ss3-ex/src/App.tsx
@@ -1,44 +1,86 @@
-import React, { Component } from "react";
+// App.tsx
+import React, { Component, ChangeEvent } from "react";
 import "./App.css";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login";
+
+interface FormState {
+  email: string;
+  password: string;
+  isRemember: boolean;
+}
+
 interface AppState {
-  isExpanded: boolean;
+  form: FormState;
+  isValid: boolean;
+  isLoggedIn: boolean;
 }
 
 class App extends Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      isExpanded: false,
+      form: { email: "", password: "", isRemember: false },
+      isValid: false,
+      isLoggedIn: false,
     };
   }
 
-  handleToggleExpand = () => {
-    this.setState({
-      isExpanded: !this.state.isExpanded,
-    });
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    this.setState(
+      (prevState) => ({
+        form: {
+          ...prevState.form,
+          [name]: value,
+        },
+      }),
+      this.checkValidForm
+    );
+  };
+
+  handleChangeCheckbox = () => {
+    const { form } = this.state;
+    form.isRemember = !form.isRemember;
+    this.setState({ form }, this.checkValidForm);
+  };
+
+  checkValidForm = () => {
+    const { email, password } = this.state.form;
+    const isValid = email && password;
+    this.setState({ isValid: !!isValid });
+  };
+
+  handleSubmit = () => {
+    if (this.state.isValid) {
+      this.setState({ isLoggedIn: true });
+    }
+  };
+
+  handleLogOut = () => {
+    this.setState({ isLoggedIn: false });
+    const { form } = this.state;
+    if (!form.isRemember) {
+      this.setState({ form: { email: "", password: "", isRemember: false } });
+    }
   };
 
   render() {
+    const { isLoggedIn, form } = this.state;
+
+    if (isLoggedIn) {
+      return <Home onLogOut={this.handleLogOut} />;
+    }
+
     return (
-      <div className="App">
-        <h1 style={{ backgroundColor: "green", color: "white" }}>
-          Condition Rendering
-        </h1>
-        {this.state.isExpanded ? (
-          <>
-            <button onClick={this.handleToggleExpand}>Đóng giới thiệu</button>
-            <h1>Giới thiệu</h1>
-            <p>
-              Trong ReactJs, đôi khi bạn có một thuộc vào từng điều kiện ví dụ
-              như trạng thái của state, props,... mà bạn muốn hiển thị một hoặc
-              một số component nào đó. Khi đó bạn có thể sử dụng Conditional
-              rendering để render ra component mà bạn mong muốn.
-            </p>
-          </>
-        ) : (
-          <button onClick={this.handleToggleExpand}>Xem giới thiệu</button>
-        )}
-      </div>
+      <Login
+        form={form}
+        onChange={this.handleChange}
+        onChangeCheckbox={this.handleChangeCheckbox}
+        onSubmit={this.handleSubmit}
+      />
     );
   }
 }
