490e3d51cff78822f12d48b22887781130eb5a40
VanThen60hz <nguyenvthang2409@gmail.com>
2023-12-06 19:38:50 +0700

    add ss4 practice useEffect

--- ss4/ss4-prac/src/App.js
+++ ss4/ss4-prac/src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/Counter";
+import Selector from "./components/Selector";
 
 function App() {
   return (
     <>
-      <Counter />
+      <Selector />
     </>
   );
 }
