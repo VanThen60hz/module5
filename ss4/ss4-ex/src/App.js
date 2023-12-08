d2c0b01b32281b2171553f1e37e7bcb25a9a222b
VanThen60hz <nguyenvthang2409@gmail.com>
2023-12-08 18:27:38 +0700

    add ss4-ex car selection

--- ss4/ss4-ex/src/App.js
+++ ss4/ss4-ex/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // const carList = [
+  //   { id: 1, name: "BMW" },
+  //   { id: 2, name: "Mercedes" },
+  //   { id: 3, name: "Audi" },
+  // ];
+
+  // const colorList = [
+  //   { id: 1, name: "Red" },
+  //   { id: 2, name: "Blue" },
+  //   { id: 3, name: "Green" },
+  // ];
+
+  const carList = ["BMW", "Mercedes", "Audi"];
+  const colorList = ["Red", "Blue", "Green"];
+
+  const [selectedCar, setSelectedCar] = useState({
+    car: carList[0],
+    color: colorList[0],
+  });
+
+  // const handleChange = (type, value) => {
+  //   setSelectedCar((prev) => {
+  //     return {
+  //       ...prev,
+  //       [type]:
+  //         type === "car"
+  //           ? carList.find((car) => car.name === value)
+  //           : colorList.find((color) => color.name === value),
+  //     };
+  //   });
+  // };
+
+  const handleChange = (type, value) => {
+    console.log(type, value);
+    setSelectedCar((prev) => {
+      return {
+        ...prev,
+        [type]: value,
+      };
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Car name: {selectedCar.car}</h1>
+      <h2>Car color: {selectedCar.color}</h2>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <select onChange={(e) => handleChange("car", e.target.value)}>
+          {carList.map((car, index) => (
+            <option key={index}>{car}</option>
+          ))}
+        </select>
+        <select onChange={(e) => handleChange("color", e.target.value)}>
+          {colorList.map((color, index) => (
+            <option key={index}>{color}</option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
